b88dd63c0cf911d5290be562042697f29e92dbcb
JoeTanto2 <77851879+JoeTanto2@users.noreply.github.com>
2021-11-06 17:13:18 -0400

    initial

---
+++ Desktop/github/CodeRhinoChat/ChatFolder/go.mod
@@ -0,0 +1,3 @@
+module github/JoeTanto2/CodeRhinoChat
+
+go 1.17
---
+++ Desktop/github/CodeRhinoChat/ChatFolder/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func check() string {
+	return "nothing"
+}
+func main() {
+	fmt.Println(check())
+}
